db48559b534d57a8e78c056deff9fd00dfbf23c2
Joel Perras <joel@nerderati.com>
2016-07-05 14:59:34 -0400

    Adds setup.py and requirements.txt for easier installation/development.

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+Wiuppy, the Where's It Up API Python library.
+
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="wiuppy",
+    version="0.0.1",
+    author="ellotheth",
+    description="The core API tools & utilities for all FK applications.",
+    packages=find_packages(),
+    long_description=__doc__,
+    zip_safe=False,
+    install_requires=[
+        'requests>=2.10'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
